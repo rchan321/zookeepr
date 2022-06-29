57d337e5926e46e62f9e630fb95610d0ffd51a1a
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-06-28 20:11:18 -0400

    animals

---
+++ server.js
@@ -0,0 +1,20 @@
+// creating a route that the front-end can request data from. Here we require the data by adding the following code to the top of server.js
+const { animals } = require('./data/animals');
+
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+//route
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+});
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
