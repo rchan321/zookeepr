b6af7164bc1f94ca0e2a95b35f975e6396427395
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-06-29 23:54:23 -0400

    lesson 1

--- server.js
+++ server.js
@@ -12,9 +12,19 @@ const app = express();
 
 
 //route
-app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
-});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
